80cc6bd9aaafb73f9ffa7886e160716866e85a67
sharan3102 <2020it0134@svce.ac.in>
2024-04-07 23:24:18 +0530

    feat(java): added easy java questions 😌

---
+++ java/containsDuplicate.java
@@ -0,0 +1,14 @@
+//  https://leetcode.com/problems/contains-duplicate/
+class Solution {
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> uniques = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (uniques.contains(nums[i])) {
+                return true;
+            }
+            uniques.add(nums[i]);
+        }
+        return false;
+    }
+}
